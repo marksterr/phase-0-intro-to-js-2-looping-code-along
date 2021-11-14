fbbc571b9c15271a06f1fa98310b4946a32c72a5
Mark <mrkdawes8@gmail.com>
2021-11-14 17:10:23 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names, adjective) {
+  let result = [];
+  for (let i = 0; i < names.length; i++) {
+    result.push(`Thank you, ${names[i]}, for the wonderful ${adjective} gift!`);
+  }
+  return result;
+}
+
+function countDown(num) {
+  for (let i = num; i > -1; i--) {
+    console.log(i);
+  }
+}
